7797b3d0f1260195245f45e30794c15a53e3e6a7
patiwwb <73791044+patiwwb@users.noreply.github.com>
2022-02-18 15:23:53 +0100

    Delete SRC directory

--- SRC/BENCH/Counter_tb_conf.vhd
+++
@@ -1,9 +0,0 @@
-library LIB_RTL;
-
-configuration Counter_tb_conf of Counter_tb is
-	for Counter_tb_arch
-		for DUT : Counter
-			use entity LIB_RTL.Counter(Counter_arch);
-		end for;
-	end for;
-end Counter_tb_conf;
